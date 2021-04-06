ff0a5a33ae382fbb99b0ec0fc4d93850b0364c4a
Lamarr Redhead-Davis <LRedhead-Davis@qa.com>
2021-04-06 13:04:46 +0100

    LRD: Finish Calculator exercise from QA Community/Operators

---
+++ src/com/QA/Calculator.java
@@ -0,0 +1,21 @@
+package com.QA;
+
+public class Calculator {
+    public static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public static double division(double num1, double num2) {
+        return num1 / num2;
+    }
+
+
+}
--- src/com/QA/Main.java
+++ src/com/QA/Main.java
@@ -3,6 +3,9 @@ package com.QA;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(Calculator.addition(3, 5));
+        System.out.println(Calculator.subtraction(5, 3));
+        System.out.println(Calculator.multiplication(3, 7));
+        System.out.println(Calculator.division(3.0, 8.0));
     }
 }
